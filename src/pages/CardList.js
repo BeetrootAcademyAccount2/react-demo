86347612ef251501c450c13dbfb9783bb2217cc4
BeetrootAcademyAccount2 <beetroot444@yahoo.com>
2024-05-25 12:10:25 +0300

    Remove initial cards

--- src/pages/CardList.js
+++ src/pages/CardList.js
@@ -4,51 +4,6 @@ import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 
 function CardList() {
-  const initialCards = [
-    {
-      id: 1,
-      group: "dogs",
-      img: "https://picsum.photos/id/237/200/300",
-      name: "John",
-      description: "Good dog",
-    },
-    {
-      id: 2,
-      group: "dogs",
-      img: "https://picsum.photos/id/237/200/300",
-      name: "Stephen",
-      description: "Bad dog",
-    },
-    {
-      id: 3,
-      group: "dogs",
-      img: "https://picsum.photos/id/237/200/300",
-      name: "Sven",
-      description: "Alright dog",
-    },
-    {
-      id: 4,
-      group: "mountains",
-      img: "https://picsum.photos/seed/picsum/200/300",
-      name: "Moutain 1",
-      description: "Cool Moutain",
-    },
-    {
-      id: 5,
-      group: "mountains",
-      img: "https://picsum.photos/seed/picsum/200/300",
-      name: "Moutain 2",
-      description: "Another Coll Mountain",
-    },
-    {
-      id: 6,
-      group: "towers",
-      img: "https://picsum.photos/id/870/200/300",
-      name: "Tower",
-      description: "Alright tower",
-    },
-  ];
-
   const [selectedCardGroup, setSelectedCardGroup] = useState("all");
   const [counter, setCounter] = useState(0);
   const [selectedCards, setSelectedCards] = useState([]);
